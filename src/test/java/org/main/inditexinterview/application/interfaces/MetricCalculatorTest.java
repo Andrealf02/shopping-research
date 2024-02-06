83d721c17e4a99dab3d9d2987aef5bf45165b55c
Andrea Franconetti <alf.franconetti@gmail.com>
2024-02-06 20:11:49 +0100

    01- create test to metrics calculator[WIP]

---
+++ src/test/java/org/main/inditexinterview/application/interfaces/MetricCalculatorTest.java
@@ -0,0 +1,40 @@
+package org.main.inditexinterview.application.interfaces;
+
+import org.junit.jupiter.api.Test;
+import org.main.inditexinterview.application.SalesUnitsMetricCalculator;
+import org.main.inditexinterview.application.StockRatioMetricCalculator;
+import org.main.inditexinterview.application.service.DefaultWeightService;
+import org.main.inditexinterview.domain.Product;
+import org.main.inditexinterview.domain.SortingCriteria;
+import org.main.inditexinterview.domain.Stock;
+import org.main.inditexinterview.domain.exception.ProductValidationException;
+import org.main.inditexinterview.domain.exception.StockValidationException;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MetricCalculatorTest {
+
+    @Test
+    public void testSalesUnitsMetricCalculator() throws StockValidationException, ProductValidationException {
+        SalesUnitsMetricCalculator calculator = new SalesUnitsMetricCalculator();
+        Product product = new Product(1, "Test Product", 50, new Stock(10, 20, 30));
+        SortingCriteria sortingCriteria = new SortingCriteria("salesUnits", Map.of("salesUnits", 1.0), new DefaultWeightService());
+
+        double result = calculator.calculateMetric(product, sortingCriteria);
+
+        assertEquals(50.0, result);
+    }
+
+    @Test
+    public void testStockRatioMetricCalculator() throws StockValidationException, ProductValidationException {
+        StockRatioMetricCalculator calculator = new StockRatioMetricCalculator();
+        Product product = new Product(2, "Test Product 2", 30, new Stock(5, 10, 15));
+        SortingCriteria sortingCriteria = new SortingCriteria("stockRatio", Map.of("stockRatio", 0.5), new DefaultWeightService());
+
+        double result = calculator.calculateMetric(product, sortingCriteria);
+
+        assertEquals(3.0, result);
+    }
+}
---
+++ src/test/java/org/main/inditexinterview/application/interfaces/WeightServiceTests.java
@@ -0,0 +1,38 @@
+package org.main.inditexinterview.application.interfaces;
+
+import org.junit.jupiter.api.Test;
+import org.main.inditexinterview.application.service.DefaultWeightService;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class WeightServiceTests {
+
+    @Test
+    public void testGetWeight() {
+        WeightService weightService = new DefaultWeightService();
+        Map<String, Double> weights = Map.of("salesUnits", 0.8, "stockRatio", 0.2);
+
+        double result = weightService.getWeight("salesUnits", weights);
+
+        assertEquals(0.8, result);
+    }
+
+    @Test
+    public void testGetWeightDefault() {
+        WeightService weightService = new DefaultWeightService();
+        Map<String, Double> weights = Map.of("stockRatio", 0.2);
+
+        double result = weightService.getWeight("salesUnits", weights);
+
+        assertEquals(1.0, result);
+    }
+
+    @Test
+    public void testValidateWeights() {
+        WeightService weightService = new DefaultWeightService();
+        Map<String, Double> weights = Map.of("salesUnits", 0.8, "stockRatio", 0.2);
+
+        assertThrows(IllegalArgumentException.class, () -> weightService.validateWeights(weights));
+    }
+}
